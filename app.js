990c96e57fe7f34be366b0e277dccce6b5607ec3
Franco Lovisolo <lovi_franco>
2023-01-27 17:50:39 -0300

    prueba 2

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hola prueba 1")
+let estudiantes = [
+    {nombre: 'John', promedio: 8.5, aprobado: true},
+    {nombre: 'Jane', promedio: 7, aprobado: true},
+    {nombre: 'June', promedio: 3, aprobado: false},
+]
+
+function filtrarAprobadosDesaprobados(alumnos){
+    let aprobados = []
+    let desaprobados = []
+    for (let alumno of alumnos) {
+        if (alumno.aprobado) {
+            aprobados.push(alumno)
+        } else {
+            desaprobados.push(alumno)
+        }
+    }
+    return [aprobados, desaprobados]
+}
+
